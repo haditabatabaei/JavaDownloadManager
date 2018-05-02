803f4b3d586f008c5931a6b15f475ab4a54f7d47
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2018-05-02 20:59:21 +0430

    Run bug fixed and a new color added.

--- src/Run.java
+++ src/Run.java
@@ -3,6 +3,5 @@ import gui.MainGui;
 public class Run {
     public static void main(String[] args) {
         MainGui javaDownloadManager = new MainGui();
-		System.out.println("FileEdited");
     }
 }
