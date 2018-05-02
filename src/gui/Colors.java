aaf3db11553d3759291e92d9dd1f0ce2d99d8853
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2018-05-02 21:09:19 +0430

    Test Color Removed.

--- src/gui/Colors.java
+++ src/gui/Colors.java
@@ -5,5 +5,4 @@ import java.awt.*;
 public abstract class Colors {
     public static final Color DarkBlue = new Color(68, 81, 153);
     public static final Color LowerDarkBlue = new Color(104, 125, 160);
-	public static final Color testColor = new Color(10,10,10);
 }
