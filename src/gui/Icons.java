cc5076ef3e62824e058f1b4205cfb0f7251af7db
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2018-05-18 00:14:17 +0430

    some changes in icons class. getter fix.

--- src/gui/Icons.java
+++ src/gui/Icons.java
@@ -2,23 +2,29 @@ package gui;
 
 import javax.swing.*;
 
+
 public class Icons {
     private final ImageIcon settings = new ImageIcon(getClass().getResource("..//icons//settings.png"));
-    private final ImageIcon settingsColor = new ImageIcon(getClass().getResource("..//icons//icons8_Settings_32px.png"));
     private final ImageIcon pause = new ImageIcon(getClass().getResource("..//icons//button_pause.png"));
-    private final ImageIcon pauseColor = new ImageIcon(getClass().getResource("..//icons//icons8_Pause_32px.png"));
     private final ImageIcon start = new ImageIcon(getClass().getResource("..//icons//button_play.png"));
-    private final ImageIcon startColor = new ImageIcon(getClass().getResource("..//icons//icons8_Circled_Play_32px.png"));
-    private final ImageIcon startColorRed = new ImageIcon(getClass().getResource("..//icons//icons8_Start_32px.png"));
+    private final ImageIcon startColoredRed = new ImageIcon(getClass().getResource("..//icons//icons8_Start_32px.png"));
     private final ImageIcon newDl = new ImageIcon(getClass().getResource("..//icons//plus_alt.png"));
-    private final ImageIcon newDlGreen = new ImageIcon(getClass().getResource("..//icons//icons8_Plus_32px_1.png"));
     private final ImageIcon newDlOrange = new ImageIcon(getClass().getResource("..//icons//icons8_Positive_32px.png"));
     private final ImageIcon delete = new ImageIcon(getClass().getResource("..//icons//trash_empty.png"));
+    private final ImageIcon littlePlay = new ImageIcon(getClass().getResource("..//icons//icons8_Circled_Play_16px_1.png"));
+    private final ImageIcon littleFolder = new ImageIcon(getClass().getResource("..//icons/icons8_Folder_16px.png"));
+    private final ImageIcon littleCancel = new ImageIcon(getClass().getResource("..//icons//icons8_Delete_16px.png"));
+    private final ImageIcon infoIcon = new ImageIcon(getClass().getResource("..//icons//icons8_Info_32px.png"));
+    private final ImageIcon pauseColor = new ImageIcon(getClass().getResource("..//icons//icons8_Pause_32px.png"));
+    private final ImageIcon settingsColor = new ImageIcon(getClass().getResource("..//icons//icons8_Settings_32px.png"));
+    private final ImageIcon startColor = new ImageIcon(getClass().getResource("..//icons//icons8_Circled_Play_32px.png"));
+    private final ImageIcon newDlGreen = new ImageIcon(getClass().getResource("..//icons//icons8_Plus_32px_1.png"));
     private final ImageIcon deletePurple = new ImageIcon(getClass().getResource("..//icons//icons8_Trash_32px.png"));
     private final ImageIcon cancelColor = new ImageIcon(getClass().getResource("..//icons//icons8_Stop_32px.png"));
     private final ImageIcon queueColor = new ImageIcon(getClass().getResource("..//icons//icons8_Future_32px.png"));
     private final ImageIcon completedColor = new ImageIcon(getClass().getResource("..//icons//icons8_Ok_32px.png"));
     private final ImageIcon processingColor = new ImageIcon(getClass().getResource("..//icons//icons8_Process_32px.png"));
+    private final ImageIcon fileIcon = new ImageIcon(getClass().getResource("..//icons//icons8_Home_45px.png"));
 
     public ImageIcon getSettings() {
         return settings;
@@ -64,10 +70,6 @@ public class Icons {
         return startColor;
     }
 
-    public ImageIcon getStartColorRed() {
-        return startColorRed;
-    }
-
     public ImageIcon getCancelColor() {
         return cancelColor;
     }
@@ -82,5 +84,29 @@ public class Icons {
 
     public ImageIcon getQueueColor() {
         return queueColor;
+    }
+
+    public ImageIcon getLittleCancel() {
+        return littleCancel;
+    }
+
+    public ImageIcon getLittleFolder() {
+        return littleFolder;
+    }
+
+    public ImageIcon getLittlePlay() {
+        return littlePlay;
+    }
+
+    public ImageIcon getInfoIcon() {
+        return infoIcon;
+    }
+
+    public ImageIcon getStartColoredRed() {
+        return startColoredRed;
+    }
+
+    public ImageIcon getFileIcon() {
+        return fileIcon;
     }
 }
