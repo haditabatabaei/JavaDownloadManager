54fc1e94b08491848f2eba2cd65b3e61db9d074c
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2018-05-25 12:30:45 +0430

    Download class fixed and can work with other classes now.

--- src/download/Download.java
+++ src/download/Download.java
@@ -1,8 +1,14 @@
-package download;
+package Download;
 
 import javax.swing.*;
 import java.io.File;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Random;
 
+/**
+ * This class represents a Download
+ */
 public class Download {
     private String fileName;
     private String urlAddress;
@@ -12,62 +18,60 @@ public class Download {
     private JProgressBar downloadProgressBar;
     private boolean isDownloading;
     private boolean isFinished;
-    private final float fullFileSize;
-    private float downloadedSize;
+    private final int fullFileSize;
+    private int downloadedSize;
     private boolean isInQueue;
     private ImageIcon fileIcon;
+    private String savePath;
+    private boolean isLaunchedAfter;
+    private boolean isSelect;
+    private boolean isPaused;
+    private boolean isCancled;
 
-    public Download(String urlAddress) {
-        this.urlAddress = urlAddress;
-        downloadProgressBar = new JProgressBar(0, 100);
-        fileName = "";
-        fullFileSize = 0;
-        fileIcon = new ImageIcon(getClass().getResource("..//icons//trash_empty.png"));
-        time = "22:01:00";
-        date = "2013-05-23";
-    }
-
-    public Download(String urlAddress, String fileName, float fullFileSize) {
-        time = "22:01:00";
-        date = "2013-05-23";
+    public Download(String urlAddress, String fileName, int fullFileSize) {
+        time = LocalTime.now().toString();
+        date = LocalDate.now().toString();
         this.urlAddress = urlAddress;
         this.fileName = fileName;
         this.fullFileSize = fullFileSize;
-        downloadProgressBar = new JProgressBar(0, 100);
+        downloadProgressBar = new JProgressBar(JProgressBar.HORIZONTAL, 0, 100);
+        downloadProgressBar.setVisible(true);
+        downloadProgressBar.setStringPainted(true);
+        int randomPercent = new Random().nextInt(100);
+        downloadProgressBar.setValue(randomPercent);
+        downloadedSize = (randomPercent * fullFileSize) / 100;
         fileIcon = new ImageIcon(getClass().getResource("..//icons//trash_empty.png"));
+        if (downloadProgressBar.getValue() == 100) {
+            isDownloading = false;
+            isFinished = true;
+        }
+        isPaused = false;
+        isCancled = false;
     }
 
-    public Download() {
-        time = "22:01:00";
-        date = "2013-05-23";
-        urlAddress = "";
-        fileName = "";
-        fullFileSize = 0;
-        downloadProgressBar = new JProgressBar(0, 100);
-        fileIcon = new ImageIcon(getClass().getResource("..//icons//trash_empty.png"));
-    }
-
-    public Download(int i) {
-        fileName = "DownloadFileName.exe";
-        urlAddress = "http://www.test.com/files/DownloadFileName.exe";
-        time = "22:01:00";
-        date = "2013-05-23";
-        downloadProgressBar = new JProgressBar(0, 100);
-/*
-        fileIcon = new ImageIcon(getClass().getResource("..//icons//trash_empty"));
-*/
-        isDownloading = true;
-        fullFileSize = 500;
-    }
-
+    /**
+     * check whether file is downloading or not
+     *
+     * @return true if it's downloading
+     */
     public boolean isDownloading() {
         return isDownloading;
     }
 
+    /**
+     * checks whether downloading is finished or not
+     *
+     * @return true if it's finished
+     */
     public boolean isFinished() {
         return isFinished;
     }
 
+    /**
+     * checks whether a Download is in queue or not
+     *
+     * @return true if it's in queue
+     */
     public boolean isInQueue() {
         return isInQueue;
     }
@@ -76,7 +80,7 @@ public class Download {
         return fullFileSize;
     }
 
-    public float getDownloadedSize() {
+    public int getDownloadedSize() {
         return downloadedSize;
     }
 
@@ -95,7 +99,7 @@ public class Download {
 
     public String toString() {
         String toReturn = "";
-        toReturn += "File Name : " + fileName + "\nurl address : " + urlAddress + "\nFile Size : " + downloadedSize;
+        toReturn += "File Name : " + fileName + "\nurl address : " + urlAddress + "\nSize: " + downloadedSize + " / " + fullFileSize;
         return toReturn;
     }
 
@@ -107,7 +111,7 @@ public class Download {
         this.fileName = fileName;
     }
 
-    public void setDownloadedSize(float size) {
+    public void setDownloadedSize(int size) {
         downloadedSize = size;
     }
 
@@ -149,5 +153,136 @@ public class Download {
 
     public void setFinished(boolean finished) {
         isFinished = finished;
+    }
+
+    public String getSavePath() {
+        return savePath;
+    }
+
+    /**
+     * sets save path
+     *
+     * @param savePath save path
+     */
+    public void setSavePath(String savePath) {
+        this.savePath = savePath;
+    }
+
+    /**
+     * checked whether file is going open
+     *
+     * @return true if option is active
+     */
+    public boolean isLaunchedAfter() {
+        return isLaunchedAfter;
+    }
+
+    /**
+     * set launch option
+     *
+     * @param launchedAfter true means file will be open after completeing Download
+     */
+    public void setLaunchedAfter(boolean launchedAfter) {
+        isLaunchedAfter = launchedAfter;
+    }
+
+    /**
+     * starts Download
+     */
+    public void start() {
+        System.out.println(toString() + " Started");
+    }
+
+    /**
+     * cancels Download
+     */
+    public void cancel() {
+        if (!isCancled) {
+            isCancled = true;
+            isPaused = true;
+            System.out.println(toString() + " Stopped/Canceled");
+        } else {
+            System.out.println("is Canceled.");
+        }
+    }
+
+    /**
+     * pause the Download
+     */
+    public void pause() {
+        if (!isPaused) {
+            isPaused = true;
+            System.out.println(toString() + " Paused");
+        } else {
+            System.out.println("is Paused Already.");
+        }
+    }
+
+    /**
+     * check whether it's paused or not
+     *
+     * @return true if it's paused.
+     */
+    public boolean isPaused() {
+        return isPaused;
+    }
+
+    /**
+     * checkes whether canceled or not
+     *
+     * @return true if canceled
+     */
+    public boolean isCancled() {
+        return isCancled;
+    }
+
+    /**
+     * removes Download
+     */
+    public void remove() {
+        System.out.println(toString() + " Removed");
+    }
+
+    /**
+     * resumes Download
+     */
+    public void resume() {
+        if (isPaused || isCancled) {
+            isPaused = false;
+            isCancled = false;
+            System.out.println(toString() + " Resumed");
+        } else {
+            System.out.println("Not paused or cancel");
+        }
+    }
+
+    /**
+     * check whether selected or not
+     *
+     * @return true / false
+     */
+    public boolean isSelected() {
+        return isSelect;
+    }
+
+    /**
+     * select Download
+     */
+    public void select() {
+        isSelect = !isSelected();
+    }
+
+    /**
+     * opens file
+     */
+    public void open() {
+        System.out.println(toString() + " Opened.");
+    }
+
+    /**
+     * open file path
+     */
+    public void openFolder() {
+        System.out.println(savePath + " directory opened.");
     }
 }
